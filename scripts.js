51f1ebab3e2a631c163c270d50608f1949c2e4ce
Mischmaschine <fixxertrixxer@gmail.com>
2023-03-18 22:54:32 +0100

    feat: design update

--- scripts.js
+++ scripts.js
@@ -10,6 +10,16 @@ const darkModeStyles = `
     --btn-bg-color-hover: #1559c1;
     --input-bg-color: #444;
   }
+
+.text-box {
+    background-color: rgba(22, 36, 46);
+    border-color: rgba(255, 255, 255, 0.1);
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+  }
+
+  body {
+    background-image: linear-gradient(to right, #1c2d3a, #2f3f57);
+  }
 `;
 
 const isSystemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
